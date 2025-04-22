637e8321d5328b8b2daa92046ace8719081c5622
Abhay Pratap <abhaypratap94310@gmail.com>
2025-04-22 22:12:59 +0530

    adding all song and songs covers and playing functionality

--- script.js
+++ script.js
@@ -2,60 +2,67 @@ let songIndex = 0;
 let audioElement = new Audio("songs/1.mp3");
 let masterPlay = document.getElementById("masterPlay");
 let myProgressBar = document.getElementById("myProgressBar");
-let gif = document.getElementById('gif')
+let gif = document.getElementById("gif");
+let songItems = Array.from(document.getElementsByClassName("songItem"));
 
 let songs = [
   {
-    songName: "Salame-e-Ishq",
+    songName: "Song1",
     filePath: "songs/1.mp3",
-    coverPath: "cover/1.jpg",
+    coverPath: "covers/1.jpg",
   },
   {
-    songName: "Salame-e-Ishq",
-    filePath: "songs/1.mp3",
-    coverPath: "cover/1.jpg",
+    songName: "Song2",
+    filePath: "songs/2.mp3",
+    coverPath: "covers/2.jpg",
   },
   {
-    songName: "Salame-e-Ishq",
-    filePath: "songs/1.mp3",
-    coverPath: "cover/1.jpg",
+    songName: "Song3",
+    filePath: "songs/3.mp3",
+    coverPath: "covers/3.jpg",
   },
   {
-    songName: "Salame-e-Ishq",
-    filePath: "songs/1.mp3",
-    coverPath: "cover/1.jpg",
+    songName: "Song4",
+    filePath: "songs/4.mp3",
+    coverPath: "covers/4.jpg",
   },
   {
-    songName: "Salame-e-Ishq",
-    filePath: "songs/1.mp3",
-    coverPath: "cover/1.jpg",
+    songName: "Song5",
+    filePath: "songs/5.mp3",
+    coverPath: "covers/5.jpg",
   },
   {
-    songName: "Salame-e-Ishq",
-    filePath: "songs/1.mp3",
-    coverPath: "cover/1.jpg",
+    songName: "Song6",
+    filePath: "songs/6.mp3",
+    coverPath: "covers/6.jpg",
   },
   {
-    songName: "Salame-e-Ishq",
-    filePath: "songs/1.mp3",
-    coverPath: "cover/1.jpg",
+    songName: "Song7",
+    filePath: "songs/7.mp3",
+    coverPath: "covers/7.jpg",
   },
   {
-    songName: "Salame-e-Ishq",
-    filePath: "songs/1.mp3",
-    coverPath: "cover/1.jpg",
+    songName: "Song8",
+    filePath: "songs/8.mp3",
+    coverPath: "covers/8.jpg",
   },
   {
-    songName: "Salame-e-Ishq",
-    filePath: "songs/1.mp3",
-    coverPath: "cover/1.jpg",
+    songName: "Song9",
+    filePath: "songs/9.mp3",
+    coverPath: "covers/9.jpg",
   },
   {
-    songName: "Salame-e-Ishq",
-    filePath: "songs/1.mp3",
-    coverPath: "cover/1.jpg",
+    songName: "Song10",
+    filePath: "songs/10.mp3",
+    coverPath: "covers/10.jpg",
   },
 ];
+
+songItems.forEach((Element, i) => {
+  // console.log(Element, i);
+  Element.getElementsByTagName("img")[0].src = songs[i].coverPath;
+  Element.getElementsByClassName("songName")[0].innerText = songs[i].songName;
+});
 
 // audioElement.play();/
 
@@ -63,20 +70,51 @@ let songs = [
 masterPlay.addEventListener("click", () => {
   if (audioElement.paused || audioElement.currentTime <= 0) {
     audioElement.play();
-    masterPlay.classList.remove('fa-play-circle')
-    masterPlay.classList.add('fa-pause-circle')
+    masterPlay.classList.remove("fa-play-circle");
+    masterPlay.classList.add("fa-pause-circle");
     gif.style.opacity = 1;
-}else{
+  } else {
     audioElement.pause();
-    masterPlay.classList.remove('fa-pause-circle')
-    masterPlay.classList.add('fa-play-circle')
+    masterPlay.classList.remove("fa-pause-circle");
+    masterPlay.classList.add("fa-play-circle");
     gif.style.opacity = 0;
-
-}
+  }
 });
 
 // listen to events
-myProgressBar.addEventListener("timeupdate", () => {
-  console.log("timeupdate");
+audioElement.addEventListener("timeupdate", () => {
   // updating seekbar
+  progress = parseInt((audioElement.currentTime / audioElement.duration) * 100);
+  myProgressBar.value = progress;
 });
+
+myProgressBar.addEventListener("change", () => {
+  audioElement.currentTime =
+    (myProgressBar.value * audioElement.duration) / 100;
+});
+
+const makeAllPlays = () =>{
+
+  Array.from(document.getElementsByClassName('songItemPlay')).forEach((Element)=>{
+  Element.classList.remove("fa-pause-circle");
+  Element.classList.add("fa-play-circle");
+
+  })
+
+}
+
+Array.from(document.getElementsByClassName('songItemPlay')).forEach((Element)=>{
+  Element.addEventListener('click', (e)=>{
+    makeAllPlays();
+    songIndex = parseInt(e.target.id);
+    Element.classList.remove("fa-play-circle");
+    Element.classList.add("fa-pause-circle");
+    audioElement.src = `songs/${songIndex+1}.mp3`;
+    audioElement.currentTime = 0;
+    audioElement.play();
+    index = parseInt(e.target.id);
+    Element.classList.remove("fa-play-circle");
+    Element.classList.add("fa-pause-circle");
+    
+  })
+})j
